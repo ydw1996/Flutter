ee596cf01b59a8b4bf0afc2360d20bf2145e3259
Dawit yang <112791174+ydw1996@users.noreply.github.com>
2023-08-25 23:14:41 +0900

    커스텀 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -31,49 +31,66 @@ class MyApp extends StatelessWidget {
               ),
             ],
         ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('dw.jpg', width: 200),
-              Container(
-                width: 250,
-                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('자전거 팝니다.', style: TextStyle(color: Colors.blue),),
-                    Text('양평 3가 영등포구'),
-                    Text('3000,0원.'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],  
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-            child: SizedBox(
-              height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.call),
-                  Icon(Icons.speaker_notes),
-                  Icon(Icons.contacts),
-                ],
-              ),
-            )
-        ),
-      )
+        body: ContList(),
+        bottomNavigationBar: BtmAppBar(),
+      ),
     );
   }
 }
 
+class ContList extends StatelessWidget {
+  const ContList({super.key});
+
+  @override // 먼저 쓰라고 명령
+  build(context) {
+    return Container(
+      height: 150,
+      padding: EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Image.asset('dw.jpg', width: 200),
+          Container(
+            width: 250,
+            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('자전거 팝니다.', style: TextStyle(color: Colors.blue),),
+                Text('양평 3가 영등포구'),
+                Text('3000,0원'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(Icons.favorite),
+                    Text('4')
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BtmAppBar extends StatelessWidget {
+  const BtmAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+        child: SizedBox(
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.call),
+              Icon(Icons.speaker_notes),
+              Icon(Icons.contacts),
+            ],
+          ),
+        )
+    );
+  }
+}
