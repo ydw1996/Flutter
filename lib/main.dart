c7cd244e4460f7e317d46a74182d150217ae21f3
Dawit yang <112791174+ydw1996@users.noreply.github.com>
2023-08-29 21:36:33 +0900

    ConList Widgets Works3

--- lib/main.dart
+++ lib/main.dart
@@ -11,59 +11,62 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text("양평3가", style: TextStyle(color: Colors.black)),
+          backgroundColor: Color(0xff333333),
+          title: Text("양평3가", style: TextStyle(color: Colors.white)),
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.search),
-              color: Colors.black,
+              color: Colors.white,
               onPressed: () {},
             ),
             IconButton(
               icon: Icon(Icons.menu),
-              color: Colors.black,
+              color: Colors.white,
               onPressed: () {},
             ),
             IconButton(
               icon: Icon(Icons.doorbell),
-              color: Colors.black,
+              color: Colors.white,
               onPressed: () {},
             ),
           ],
         ),
-        body: ListView(
-          children: [
-            ContList(
-              title: '에어팟 왼쪽 팝니다',
-              location: '당산동2가',
-              price: '90,000원',
-              favoriteCount: 7,
-            ),
-            ContList(
-              title: '애플워치 SE 셀룰러 팝니다',
-              location: '강남구 서초동',
-              price: '500,000원',
-              favoriteCount: 7,
-            ),
-            ContList(
-              title: '먹태깡 팝니다',
-              location: '당산동6가',
-              price: '3,000원',
-              favoriteCount: 7,
-            ),
-            ContList(
-              title: '자전거 팝니다~',
-              location: '양천구 목1동',
-              price: '500,000원',
-              favoriteCount: 7,
-            ),
-            ContList(
-              title: '닌텐도 스위치 그레이 플박 급처',
-              location: '양평동',
-              price: '200,000원',
-              favoriteCount: 7,
-            ),
-          ],
+        body: Container(
+          color: Color(0xff333333),
+          child: ListView(
+            children: [
+              ContList(
+                title: '에어팟 왼쪽 팝니다',
+                location: '당산동2가',
+                price: '90,000원',
+                favoriteCount: 7,
+              ),
+              ContList(
+                title: '애플워치 SE 셀룰러 팝니다',
+                location: '강남구 서초동',
+                price: '500,000원',
+                favoriteCount: 7,
+              ),
+              ContList(
+                title: '먹태깡 팝니다',
+                location: '당산동6가',
+                price: '3,000원',
+                favoriteCount: 7,
+              ),
+              ContList(
+                title: '자전거 팝니다~',
+                location: '양천구 목1동',
+                price: '500,000원',
+                favoriteCount: 7,
+              ),
+              ContList(
+                title: '닌텐도 스위치 그레이 플박 급처',
+                location: '양평동',
+                price: '200,000원',
+                favoriteCount: 7,
+              ),
+            ],
+          ),
         ),
         bottomNavigationBar: BtmAppBar(),
       ),
@@ -76,53 +79,77 @@ class ContList extends StatelessWidget {
   final String location;
   final String price;
   final int favoriteCount;
+  final bool isFirst; // 첫 번째 위젯 여부
 
   const ContList({
     required this.title,
     required this.location,
     required this.price,
     required this.favoriteCount,
+    this.isFirst = false,
     Key? key,
-}) : super(key: key);
+  }) : super(key: key);
 
   @override
   build(context) {
     return Container(
       height: 150,
-      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+      decoration: BoxDecoration(
+        border: isFirst
+            ? Border(
+                top: BorderSide(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+              )
+            : Border(
+                bottom: BorderSide(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+              ),
+      ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             width: 120,
             height: 120,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              image: AssetImage('dw.jpg'),
-              fit: BoxFit.cover,
-            )
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage('dw.jpg'),
+                  fit: BoxFit.cover,
+                )),
           ),
-          ),
-        Expanded(
-          child: Container(
-            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(title, style: TextStyle(color: Colors.blue),),
-                Text(location),
-                Text(price),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Icon(Icons.favorite),
-                    Text(favoriteCount.toString())
-                  ],
-                )
-              ],
+          Expanded(
+            //
+            child: Container(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title, style: TextStyle(color: Colors.white)),
+                  Text(location, style: TextStyle(color: Colors.white)),
+                  Text(price, style: TextStyle(color: Colors.white)),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(
+                          Icons.favorite_border,
+                          color: Colors.white,
+                        ),
+                        Text(favoriteCount.toString(),
+                            style: TextStyle(color: Colors.white))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
         ],
       ),
     );
@@ -134,18 +161,31 @@ class BtmAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey,
+            width: 1.0,
+          ),
+        ),
+      ),
+      child: BottomAppBar(
+        color: Color(0xff333333),
         child: SizedBox(
-          height: 50,
+          height: 60,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Icon(Icons.call),
-              Icon(Icons.speaker_notes),
-              Icon(Icons.contacts),
+              Icon(Icons.home, color: Colors.white),
+              Icon(Icons.location_city_outlined, color: Colors.white),
+              Icon(Icons.map_outlined, color: Colors.white),
+              Icon(Icons.chat_bubble_outline_rounded, color: Colors.white),
+              Icon(Icons.contacts_outlined, color: Colors.white),
             ],
           ),
-        )
+        ),
+      ),
     );
   }
 }
